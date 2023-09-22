748e37674d83a4c084b4e2cd6ad662a27d4b2acf
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-09-22 10:37:47 +0200

    feat: create authoziation config with tutorial config for service
    Refs: #SFB-19

---
+++ config/authorization/config.ex
@@ -0,0 +1,31 @@
+alias Acl.Accessibility.Always, as: AlwaysAccessible
+alias Acl.GraphSpec.Constraint.Resource, as: ResourceConstraint
+alias Acl.GraphSpec, as: GraphSpec
+alias Acl.GroupSpec, as: GroupSpec
+alias Acl.GroupSpec.GraphCleanup, as: GraphCleanup
+
+defmodule Acl.UserGroups.Config do
+  def user_groups do
+    [
+      # // PUBLIC
+      %GroupSpec{
+        name: "public",
+        useage: [:read],
+        access: %AlwaysAccessible{},
+        graphs: [ %GraphSpec{
+                    graph: "http://mu.semte.ch/graphs/public",
+                    constraint: %ResourceConstraint{
+                      resource_types: [
+                      ]
+                    } } ] },
+
+      # // CLEANUP
+      #
+      %GraphCleanup{
+        originating_graph: "http://mu.semte.ch/application",
+        useage: [:write],
+        name: "clean"
+      }
+    ]
+  end
+end
