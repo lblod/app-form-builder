f4923eec2c10f0029c4bca546ea8d7a93c517147
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-12-06 14:55:25 +0100

    SFB-100: create a separate file for each domain model

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,42 +1,11 @@
 (in-package :mu-cl-resources)
 
-(read-domain-file "concept-scheme.lisp")
-
 (defparameter *include-count-in-paginated-responses* t
   "when non-nil, all paginated listings will contain the number
    of responses in the result object's meta.")
 
-(define-resource generated-form ()
-  :class (s-prefix "ext:GeneratedForm")
-  :properties `((:created :string ,(s-prefix "dct:created"))
-                (:modified :string ,(s-prefix "dct:modified"))
-                (:label :string ,(s-prefix "skos:prefLabel"))
-                (:comment :string ,(s-prefix "skos:comment"))
-                (:ttl-code :string , (s-prefix "ext:ttlCode")))
-  :resource-base (s-url "http://data.lblod.info/generated-forms/")
-  :features `(include-uri)
-  :on-path "generated-forms")
-
-(define-resource file ()
-  :class (s-prefix "nfo:FileDataObject")
-  :properties `((:filename :string ,(s-prefix "nfo:fileName"))
-                (:format :string ,(s-prefix "dct:format"))
-                (:size :number ,(s-prefix "nfo:fileSize"))
-                (:extension :string ,(s-prefix "dbpedia:fileExtension"))
-                (:created :datetime ,(s-prefix "nfo:fileCreated")))
-  :has-one `((file :via ,(s-prefix "nie:dataSource")
-                   :inverse t
-                   :as "download"))
-  :resource-base (s-url "http://data.example.com/files/")
-  :features `(include-uri)
-  :on-path "files")
-
-(define-resource user-test ()
-  :class (s-prefix "ext:UserTest")
-  :properties `((:created :datetime ,(s-prefix "dct:created"))
-                (:modified :datetime ,(s-prefix "dct:modified")))
-  :has-one `((generated-form :via ,(s-prefix "dct:source")
-                   :as "form"))
-  :resource-base (s-url "http://data.lblod.info/user-tests/")
-  :features `(include-uri)
-  :on-path "user-tests")
+(read-domain-file "generated-form.lisp")
+(read-domain-file "user-test.lisp")
+(read-domain-file "file.lisp")
+(read-domain-file "concept-scheme.lisp")
+  
